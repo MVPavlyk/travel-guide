28e292c14067990693d64a1f5691ea278b04b74d
mykola.pavlyk <mykola.pavlyk@otakoyi.com>
2026-02-21 18:20:45 +0200

    feat: turborepo packages

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,5 +1,14 @@
-export default {
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {
+      base: path.resolve(__dirname, ".."),
+    },
   },
 };
+
+export default config;
